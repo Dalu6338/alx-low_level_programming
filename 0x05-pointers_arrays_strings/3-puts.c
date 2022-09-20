f9e3fc52a04ea274d396341fcfa1d30e1bb208fb
Dalu6338 <echemce13289@gmail.com>
2022-09-19 21:40:29 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: input string
+ * Return: no return
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_puts(*str++);
+
+	_putchar('\n');
+}
